7ee40e383e7b805395682382a179bcacf3ac3455
katerina-arkhipenkova <katerina.arkhipenkova@yandex.ru>
2022-09-18 21:29:13 +0300

    first commit

---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,29 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwnerOrReadOnly
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {'creator',}
+    filterset_class = AdvertisementFilter
+    permission_classes = [IsOwnerOrReadOnly]
+
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsOwnerOrReadOnly()]
+        return []
